f18b86f5a91ee93b7942d9b49e24f9cdc1fc0fc9
BX41FORCE <bx41force@gmail.com>
2019-02-12 00:28:16 -0500

    Adición de footer

--- BuyOn/BuyOnClient/src/app/app.module.ts
+++ BuyOn/BuyOnClient/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from '@angular/http';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
